85a6d705c21a46d5f42227b5a227765eb4b99a34
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:18:24 +0530

    TypeCasting

---
+++ typeCasting.py
@@ -0,0 +1,14 @@
+# #implicit type casting
+# a=78
+# print(a,type(a))
+# b=9.9
+# print(b,type(b))
+# c=a+b
+# print(c,type(c))
+# #explicit type casting 
+# d='67'
+# print(d,type(d))
+# e='87'
+# print(e,type(e))
+# f=int(d)+int(e)
+# print(f,type(f))
