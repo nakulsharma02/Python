839bdc534dae8fa6b4bfac32562497c51d69ae4a
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:13:44 +0530

    List

---
+++ list.py
@@ -0,0 +1,44 @@
+# list = ['this',89,True]#list are mutable
+# print(type(list))
+# #list function
+# print('this' in list)#standalone or membership operator
+
+#list methods
+list = ['this',89,True]
+list.append('your\'s')#add an item at last
+print(list)
+# list.extend([6,7])#extend the items at last 
+# print(list)
+list.insert(2,'nakul')#insert the element at a paricular index
+# print(list)#reverse the elements of the list
+list.reverse()
+print(list)
+# list1=list.copy()#copy the list and insert in another list 
+# print(list1)
+# list2=[3,5,7,3,6,7]
+# print(list2)
+# list2.sort()#Arrange in an asscending order,only applicaple fo single datatype elements
+# print(list2)
+# print(list2.count(3))#count the repetion of item
+# print(list.index(True))#give the index/position of the item
+# list.remove('nakul')#remove the given item
+# print(list)
+# list.pop()#removes the last item and return it
+# print(list)
+# list.pop(1)#removes the item according to the index
+# print(list)
+# list.clear()#clear all items from the list
+# print(list)
+# print(type(list))
+
+# # list functions 
+# print(len([1, 2, 3]))
+# print(sum([1, 2, 3]))
+# print(sorted([3, 1, 2]))
+# print(max([3, 1, 2]))
+# print(min([3, 1, 2]))
+# print(any([True,False]))
+# print(all([True,False]))#true if all elements are true
+# print(tuple([3, 1, 2]))
+# print(set([3,2,'this']))
+# print(dict([(1, 'a'), (2, 'b')]))
