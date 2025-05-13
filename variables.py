cf5e052593d46973d86460c136ca7e254533e3f0
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:18:53 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+#variable is a container which store data of any type
+# a = 78
+# print(a)
+# b = "This is your turn"
+# print(b)
+#where a and b are operands
+#and 78 & "This is your turn" are the data or values which is stored in a variable
+# Note: variable can't start with number Any kind of symbol except underscore
