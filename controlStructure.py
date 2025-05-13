275292bfa2275e317990cd54b72e50f87cfa85ca
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:11:38 +0530

    ControSt

---
+++ controlStructure.py
@@ -0,0 +1,50 @@
+# if Structure
+# a = int(input("Enter the code"))
+# if a == 45:
+#     print("Code Access")
+
+# if-else Structure
+# b = int(input("Enter the Code"))
+# if b == 5:
+#     print("Code Access")
+# else:
+#     print("Code Denied")
+
+# if-else ladder Structure
+# c = int(input("Enter Any Value"))
+# if c == 56:
+#     print("You Are Awesome")
+# elif c == 89:
+#     print("You Are Mad")
+# elif c == 98:
+#     print("Happy Journey")
+# else:
+#     print("Value denied")
+
+# use of match structure
+# a = int(input("enter the value of a"))
+# b = int(input("enter the value of b"))
+# op = input("Enter the operator")
+# match op:
+#     case '+':
+#         print(a+b)
+#     case '-':
+#         print(a-b)
+#     case '*':
+#         print(a*b)
+#     case '/':
+#         print(a/b)
+#     case '%':
+#         print(a%b)
+#     case '**':
+#         print(a**b)
+#     case '//':
+#         print(a//b)
+    
+# use of Break
+# for number in range(10):
+#     if number > 5:
+#         break  # Exit the loop when number is 5
+#     print(number)
+
+# print("Loop exited")
