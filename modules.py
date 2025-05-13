b307aa13940241f5adf069b59b5a0a02f2d6513f
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:14:27 +0530

    Modules

---
+++ modules.py
@@ -0,0 +1,34 @@
+# import pyjokes
+# joke = pyjokes.get_joke()
+# print(joke)
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Nakul Sharma")
+# engine.runAndWait()
+# from turtle import *
+# for steps in range(100):
+#     for c in ('red','green','orange'):
+#         color(c)
+#         forward(steps)
+#         right(30)
+# import os
+
+# def list_directory_contents(directory_path):
+#     try:
+#         # Get the list of files and directories in the specified directory
+#         contents = os.listdir(directory_path)
+        
+#         print(f"Contents of directory '{directory_path}':")
+#         for item in contents:
+#             print(item)
+    
+#     except FileNotFoundError:
+#         print(f"The directory '{directory_path}' does not exist.")
+#     except PermissionError:
+#         print(f"Permission denied to access the directory '{directory_path}'.")
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+# # Example usage:
+# directory_path = '/Users'  # Change this to the directory you want to list
+# list_directory_contents(directory_path)
