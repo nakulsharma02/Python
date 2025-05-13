4da9b175211c45d62bba2fb4dd0c5ffc45043c3a
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:09:26 +0530

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+# Numeric Datatype
+# a = 6
+# print(type(a))#int datatype
+# b = 9.8 
+# print(type(b))#floating point numbers
+# k = 2 + 3j
+# print(k) # complex numbers
+# d = True
+# print(type(d))#boolean data type note-always start value with caps letter
+# c = "this is type"
+# print(type(c))#string #sequence 
+# e = {'this','you','your'}
+# print(type(e))#set data type
+# f = ('this','you','your')
+# print(type(f))#tuple #sequence
+# g = ['this','you','your']
+# print(type(g))#list #sequence
+# h = {
+#     'name' : 'your',
+#     'value' : 'you'
+# }
+# print(type(h))#dictionary #mapping Datatype
+# i = None
+# print(type(i))#None DataType
+##Bool Datatypes
+a=bool(5)
+print(a)
+##Binary Datatypes
+#bytes
+x=bytes(5)
+print(x)
+y=bytearray(5)
+print(y)
+z=memoryview(bytes(5))
+print(z) 
