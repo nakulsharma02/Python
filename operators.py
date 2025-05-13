914fdf27fadfd761fc73ac1b5036c293fa8df225
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:10:04 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,49 @@
+# # Arithmetic operators(+,-,*,/,**,//)
+# # Assignment operators(=,+=,-=,*=,/=,**=,//=)
+# # Conditional operators(==,!=,>=,<=)
+# # logical operators(use of and or not)
+
+# # -->Arithmetic Operator
+# a = 45
+# b = 89
+# print(a+b)#Addition operator
+# print(a-b)#Subtration Operator
+# print(a*b)#Multiply Operator
+# print(a/b)#Division Operator
+# print(a**b)#Exponential operator
+# print(a//b)#Foat division operator
+# -->Assignment Operator
+# a = 2
+# b = 4
+# print(a)#Equal Operator
+# b += a
+# print(b)#Increment Operator
+# b -= a
+# print(b)#Decrement Operator
+# b *= a
+# print(b)
+# b /= a
+# print(b)
+# b **= a
+# print(b)
+# b //= a
+# print(b)
+# -->Conditional Operators
+# a = 45
+# b = 56
+# print(a==b)
+# print(a<=b)
+# print(a>=b)
+# print(a!=b)
+# -->Logical Operators
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+# print(not(a))
+#membership 
+#in
+# for x in range(5):
+#     print(x)
+
+
