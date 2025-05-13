886bb0e249620ab889e3f2ef70586f0230f7094c
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-13 18:17:50 +0530

    Tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# a=('this','ui',7,8,8,9,7,9,8,8978,True)#tuples are immutable
+# print(a,type(a))
+# #tuple methods
+# print(a.index('ui'))
+# print(a.count(8))
+# #tuple functions
+# print(len((1, 2, 3)))
+# print(tuple(reversed((1, 2, 3))))
+# print(sum((1, 2, 3)))
+# print(tuple([1, 2, 3]))
