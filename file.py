3a95f3a391ee7f40a6c1d97942994401569b5dd4
nakulsharma02 <nakulsharma123@gmail.com>
2025-08-04 08:11:33 -0700

    File Crud Operations

--- file.py
+++ file.py
@@ -1,15 +1,39 @@
-# r = open('text.txt')
-# data = r.read()
-# import pyttsx3
-# engine=pyttsx3.init()
-# engine.say(data)
-# engine.runAndWait()
-# r.close()
-# st=input("Give Your intro here")
-# w = open('text.txt','w')
-# w.write(st)
-# w.close()
+# files Crud Operations
 
-##The same can be written using 'with' statement like this:
-with open("text.txt") as f:
-    print(f.read())
+# Write And Update Operations
+'''
+f = open("D://Python//Files/file1.txt","w")
+f.write("Hello This is the test of Overlapping")
+f.close()
+'''
+'''
+f= open("D://Python//Files/file1.txt","a")
+f.write("\nThis is the test of Append Operation")
+f.close()
+'''
+'''f= open("D://Python//Files/file1.txt","w+")
+a = 20 
+b = 80
+lambda_function = lambda x, y: x + y
+f.write(lambda_function(a, b).__str__())
+f.seek(0)
+print(f.read())
+f.close()'''
+
+# Read Operations
+'''f = open("D://Python//Files/file1.txt","r")
+read = f.read()
+print(read)
+f.close()'''
+
+'''f= open("D://Python//Files/file1.txt","a+")
+f.write("This is a new line for word count.\n")
+f.seek(0)
+read = f.readlines()
+for line in read :
+    print(line + " ")
+    words  = line.split(" ")
+    count = len(words)
+    print("Number of words in line:", count)
+f.close()
+'''
