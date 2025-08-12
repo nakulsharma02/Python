1a9a7d6b69062963913bfa952e2a464e717084fd
nakulsharma02 <nakulsharma123@gmail.com>
2025-08-12 11:19:27 -0700

    Fixing some logiacal errors realted to object and self(Parameter)

--- Advance Python/Inheritance.py
+++ Advance Python/Inheritance.py
@@ -4,7 +4,7 @@ class Human:
         print(f"I am a {self.human}, this is a {human} class")
 class human1(Human):
     def __init__(self,name):
-        Human.__init__("human1")
+        Human.__init__(self,"human1")
         self.name = name
         print(f"My name is {self.name}")
     def age(self, age):
@@ -12,20 +12,19 @@ class human1(Human):
         print(f"My age is {self.age}")
 class human2(Human):
     def __init__(self,name):
-        Human.__init__("human2")   
+        Human.__init__(self,"human2")   
         self.name = name
         print(f"My name is {self.name}")
     def age(self, age):
         self.age = age
         print(f"My age is {self.age}")
-Human = Human("Human")
+Object = Human("Human")
 ramesh = human1("Ramesh")
 ramesh.age(25)
 suresh = human2("Suresh")
 suresh.age(30)
 isinstance(ramesh, Human)  # True, It is a fumction to check whether an object is an instance of a class or a subclass.
 issubclass(human1, Human)  # True, It is a function to check whether a class is a subclass of another class.
-
 # Output:
 # I am a human
 # My name is Ramesh
