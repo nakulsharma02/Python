49de7bef9d8e6f4efb715e768b63a13c99752e75
nakulsharma02 <nakulsharma123@gmail.com>
2025-10-06 00:44:15 +0530

    MultiProcessing In Python

---
+++ Advance Python/MultiProcessing.py
@@ -0,0 +1,44 @@
+# Computer's CPU handles Multiple Processes at the same time this process is called MultiProcessing on exection
+'''import time
+import multiprocessing
+square_result = []
+def calc_square(numbers):
+    global square_result
+    for n in numbers:
+        print('Square'+str(n*n))
+        square_result.append(n*n)
+        time.sleep(5)
+    print('within a process:result'+str(square_result))
+if __name__ == "__main__":
+    arr = [2,3,8,9]
+    p1 = multiprocessing.Process(target=calc_square,args=(arr,))
+    p1.start()
+    p1.join()
+    print('result'+str(square_result))
+'''
+
+# Sharing Data Btw Processes using Array and Value
+# with the help of array
+'''import multiprocessing
+def calc_square(numbers,result):
+    for idx,n in enumerate(numbers):
+        result[idx]=n*n
+if __name__ == "__main__":
+    numbers = [2,3,5]
+    result = multiprocessing.Array('i',3)
+    p = multiprocessing.Process(target = calc_square,args=(numbers,result))
+    p.start()
+    p.join()
+    print(result[:])
+    '''
+
+# with the help of values 
+import multiprocessing 
+def calc_square(numbers):
+    numbers.value=5.67
+if __name__ =="__main__":
+    numbers = multiprocessing.Value('d',0.0)
+    p = multiprocessing.Process(target=calc_square,args=(numbers,))
+    p.start()
+    p.join()
+    print(numbers.value)
