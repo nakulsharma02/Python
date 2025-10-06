24247fecf56648121749d2dfb53b7caceffce8f6
nakulsharma02 <nakulsharma123@gmail.com>
2025-10-06 22:31:28 +0530

    Updated

--- Advance Python/MultiProcessing.py
+++ Advance Python/MultiProcessing.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     '''
 
 # with the help of values 
-import multiprocessing 
+'''import multiprocessing 
 def calc_square(numbers):
     numbers.value=5.67
 if __name__ =="__main__":
@@ -42,3 +42,19 @@ if __name__ =="__main__":
     p.start()
     p.join()
     print(numbers.value)
+'''
+
+# with the help of MultiProcessing Queue in Python
+import multiprocessing
+def calc_square(numbers,q):
+    for n in numbers:
+        q.put(n*n)
+if __name__ == "__main__":
+    numbers = [2,3,5]
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=calc_square,args=(numbers,q))
+    p.start()
+    p.join()
+
+    while q.empty() is False:
+        print(q.get())
