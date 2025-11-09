1d707b2a4bce05a8449546c8e071068883edbfd8
nakulsharma02 <nakulsharma123@gmail.com>
2025-11-09 18:23:21 +0530

    Matrix in Numpy

---
+++ Numpy/Matrix.py
@@ -0,0 +1,8 @@
+# Matrix in numpy 
+# We can also use numpy array as matrix
+import numpy as np
+matrix = np.matrix([[1,2],[2,3],[3,4]])
+print(matrix)
+print(matrix.dtype)
+print(type(matrix))
+print(np.result_type(matrix))
