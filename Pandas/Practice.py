b43b7bbada3971dc19a0de8e8e1059dcc6d58436
nakulsharma02 <nakulsharma123@gmail.com>
2025-11-11 23:51:14 +0530

    Practice

--- Pandas/Practice.py
+++ Pandas/Practice.py
@@ -9,3 +9,19 @@ df = pd.DataFrame(data)
 # Display the data 
 print("Sample Data :")
 print(df)
+print("Names(Single column return series)")
+print(df['Name'])
+# Selecting Multiple Columns
+subset = df[['Name','Age']]
+print(subset)
+# Filtering of Rows
+high_salary = df[df['Salary']>400000]
+print('Employees with Salary > 400000')
+print(high_salary)
+# Filterzing Rows with multiple conditions 
+filtered = df[(df['Age']<30)&(df['Salary']>=500000)]
+print(f'Employees list Age < 30 + Salary >400000')
+print(filtered)
+# Using OR Condition 
+filtered_or = df[(df['Age']>28)|(df['Performance Ratio']>=85)]
+print(filtered_or)
